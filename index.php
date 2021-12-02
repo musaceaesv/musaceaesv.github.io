3fd7d776046ffd7dec431eb41f1fec80a9383b12
musaceaesv <cus100158@gmail.com>
2021-12-02 03:21:18 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="font.css">
+	<link rel="stylesheet" type="text/css" href="main.css">
+
+	<link rel="stylesheet" type="text/css" href="index.css">
+
+	<link rel="icon" type="icon/png" href="https://img2.freepng.es/20180512/rqq/kisspng-computer-icons-banana-clip-art-5af6d47e0679f8.0226329415261256940265.jpg">
+
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/style.css">
+
+	
+	<title>MUSACEAE</title>
+</head>
+	<body>
+		<div class="social-bar">
+			<a href="https://www.facebook.com" class="icon icon-facebook" target="_blank"></a>
+    <a href="https://twitter.com" class="icon icon-twitter" target="_blank"></a>
+    <a href="https://www.youtube.com" class="icon icon-youtube" target="_blank"></a>
+    <a href="https://www.instagram.com" class="icon icon-instagram" target="_blank"></a>
+			
+		</div>
+		<div class="contenedor">
+
+			<header class="header">
+				<img src="recursos/logo1.jpeg">
+				<h1>¡Bienvenidos! <br>
+				MUSACEAE SA. DE CV.</h1>
+
+				
+			</header>
+			<section class="main">
+				<h3>¿Quienes somos?
+					</h3>
+					<br>
+					<p>Somos una empresa salvadoreña que se dedica  a la cultivacion, cosecha y distribución de platano en todo el país
+						 y tenemos como objetivo, abastecer principalmente a los pequeños comerciantes de platano tales como bodegas, mercados municipales y tiendas de mayores de platano en los diferentes municipios del país.</p> <br>
+				<img src="recursos/cultivo1.jpg">
+				<img src="recursos/cultivo2.jpg">
+				<img src="recursos/cultivo3.jpg">
+				<img src="recursos/cultivo4.jpg">
+
+			</section>
+			<aside class="aside">
+				<div class="container">
+            <div class="row">
+                
+                <div class="two columns u-pull-right">
+                    <ul>
+                        <li class="submenu">
+                            
+                            <div id="carrito">
+                                
+                                <table id="lista-carrito" class="u-full-width">
+                                    
+                                    <tbody>
+                                        
+                                    </tbody>
+                                </table>
+                                <a href="#" id="vaciar-carrito"
+                                class="button u-full-width">vaciar carrito</a>
+
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+				<div id="lista-platillos" class="container">
+        <h1 class="encabezado">Comprar producto</h1>
+        	<img src="img/cart.png" alt="">
+        <div class="row">
+            <div class="four columns">
+                <div class="card">
+                    <img src="img/platillo1.jpg" class="imagen-platillo">
+                    <div class="info-card">
+                        <h4>Ciento de plátano grande.</h4>
+               
+                        <img src="img/estrellas.png">
+                        <p class="precio">$28.00  </p>
+                        <a href="#" class="u-full-width button-primary button input agregar-carrito" data-id="1">Agregar al carrito</a>
+                    </div>
+                </div>
+            </div>
+             </div>
+
+    </div>
+				
+			
+				<br>
+				<br>
+				
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<div class="ub">
+					<h3>Ubicación</h3>
+				
+				<p> Cantón EL Majahual, lotificacion El Tecolote, calle principal contiguo a casa comunal, lote sin numero, La Libertad.</p>
+				<br>
+				
+				</div>
+				<br>
+				<p>Google maps.</p>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1183.3449720126425!2d-89.36817217077473!3d13.495408303482215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xed1a1525d28669b6!2zMTPCsDI5JzQzLjUiTiA4OcKwMjInMDMuNSJX!5e1!3m2!1ses!2ssv!4v1638405469141!5m2!1ses!2ssv" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br> 
+					<div class="mi">
+						<h3>Misión</h3>
+						
+					<p class="pm"> 
+						Somos una empresa comprometida con la alimentación de los salvadoreños, dando así en todo momento lo mejor de nosotros. 
+						Cosechando productos de la mas alta calidad y bajo estricta supervisión. Garantizando así que usted lleve a su paladar, el mejor plátano producido en el salvador. <br>
+						¡Un producto cultivado por manos tan salvadoreñas como tu!.
+
+					</p>
+					</div>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+					<div class="vi">
+						<h3>Visión</h3>
+					
+					<p>
+						Ofrecer, mas que un producto, una verdadera experiencia de calidad culinaria.
+						Enarbolando con ello el nombre de nuestro país ante los consumidores
+						satisfacer las necesidades de los clientes de todas las edades
+						Generar fuentes de empleo y mantener nuestra responsabilidad social empresarial en cada momento del proceso de cultivo y producción.
+
+					</p>
+					</div>
+				
+				<br>
+				<br>
+				<br>
+				<br><br>
+				<br>
+				<br><br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				
+				
+				<br>
+			</aside>
+
+			<footer>
+				<p>Contáctenos 2216-8564 / 78654258 <br>
+				Todos los derechos reservados. 2021 ®</p>
+			</footer>
+
+
+		</div>
+		<script src="js/app.js"></script>
+	</body>
+</html>
